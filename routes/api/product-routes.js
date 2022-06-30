83cf68204da168edb0441a576f4c04eec276113e
Jean Lee <jeanlee.jingli@icloud.com>
2022-06-30 14:43:53 -0700

    defined the router get

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,19 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/',  async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll(
+      {
+        include: [{ model: Category }, { model: Tag }]
+      }
+    );
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
@@ -91,6 +101,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+
 });
 
 module.exports = router;
